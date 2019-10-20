e351d326063d20e1b97084227f438aaab8881e08
TransientCW <chris@myubercode.com>
2019-10-20 16:50:33 -0600

    bubble sort

--- SortAlgos/src/com/wilson/sorting/BubbleSort.java
+++ SortAlgos/src/com/wilson/sorting/BubbleSort.java
@@ -5,16 +5,22 @@ public class BubbleSort {
 		int index = 0;
 		int unsortedPartitionIndex = unsorted.length - 1;
 		
+		// While the unsortedPartitionIndex is greater than 1 (second index)
 		while(unsortedPartitionIndex >= 1) {
+			// Traverse left to right until the index is still less than unsortedPI
 			while(index < unsortedPartitionIndex) {
+				// Swap indices if applicable
 				if (unsorted[index] > unsorted[index + 1]) {
 					int placeholder = unsorted[index];
 					unsorted[index] = unsorted[index + 1];
 					unsorted[index + 1] = placeholder;
 				}
+				// keep increasing index until its just under unsortedPI
 				index++;
 			}
+			// Reset index to 0, to get ready for next traversal
 			index = 0;
+			// Decrease unsortedPI, and keep doing so while greater than 1
 			unsortedPartitionIndex--;
 		}
 		return unsorted;
--- SortAlgos/src/com/wilson/sorting/SortEntry.java
+++ SortAlgos/src/com/wilson/sorting/SortEntry.java
@@ -6,9 +6,11 @@ public class SortEntry {
 		
 		// BUBBLE SORT
 		int[] unsorted1 = new int[] {55, 2, 29, -1, 0, 11, 27, -13, 18};
-		int[] sorted = BubbleSort.sort(unsorted1);
-		System.out.println("Sorted: " + BubbleSort.toString(sorted));
-		
+		int[] unsorted2 = new int[] {123, -22, -76, 49, 0, 33, 47, 1, 14, -3, 90, 18};
+		int[] sorted1 = BubbleSort.sort(unsorted1);
+		int[] sorted2 = BubbleSort.sort(unsorted2);
+		System.out.println("Sorted 1: " + BubbleSort.toString(sorted1));
+		System.out.println("Sorted 2: " + BubbleSort.toString(sorted2));
 		//
 	}
 
