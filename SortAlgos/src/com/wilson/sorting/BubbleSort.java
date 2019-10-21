2f478cbdf6b1d53ecc586736046b61ae335549c2
TransientCW <chris@myubercode.com>
2019-10-20 18:41:16 -0600

    finished bubble sort with ascending or descending options

--- SortAlgos/src/com/wilson/sorting/BubbleSort.java
+++ SortAlgos/src/com/wilson/sorting/BubbleSort.java
@@ -1,30 +1,50 @@
 package com.wilson.sorting;
 
 public class BubbleSort {
-	public static int[] sort(int[] unsorted) {
-		int index = 0;
-		int unsortedPartitionIndex = unsorted.length - 1;
+	public static int[] sort(int[] unsorted, String direction) {
+		int index = direction.toLowerCase() == "ascending" ? 0 : unsorted.length - 1;
+		int unsortedPartitionIndex = direction.toLowerCase() == "ascending" ? unsorted.length - 1 : 0;
+				
+		return direction.toLowerCase() == "ascending" ? sortAscending(unsorted, index, unsortedPartitionIndex) : sortDescending(unsorted, index, unsortedPartitionIndex);
 		
-		// While the unsortedPartitionIndex is greater than 1 (second index)
+	};
+	
+	private static int[] sortAscending(int[] unsorted, int index, int unsortedPartitionIndex) {
 		while(unsortedPartitionIndex >= 1) {
-			// Traverse left to right until the index is still less than unsortedPI
 			while(index < unsortedPartitionIndex) {
-				// Swap indices if applicable
 				if (unsorted[index] > unsorted[index + 1]) {
-					int placeholder = unsorted[index];
-					unsorted[index] = unsorted[index + 1];
-					unsorted[index + 1] = placeholder;
+					swap(unsorted, index, index + 1);
 				}
-				// keep increasing index until its just under unsortedPI
 				index++;
 			}
-			// Reset index to 0, to get ready for next traversal
 			index = 0;
-			// Decrease unsortedPI, and keep doing so while greater than 1
 			unsortedPartitionIndex--;
 		}
 		return unsorted;
-	};
+	}
+	
+	private static int[] sortDescending(int[] unsorted, int index, int unsortedPartitionIndex) {
+		while(unsortedPartitionIndex <= unsorted.length - 1) {
+			while(index > unsortedPartitionIndex) {
+				if (unsorted[index] > unsorted[index - 1]) {
+					swap(unsorted, index, index - 1);
+				}
+				index--;
+			}
+			index = unsorted.length - 1;
+			unsortedPartitionIndex++;
+		}
+		return unsorted;
+	}
+	
+	private static void swap(int[] array, int i, int j) {
+		if (i == j) {
+			return;
+		}
+		int placeholder = array[i];
+		array[i] = array[j];
+		array[j] = placeholder;
+	}
 	
 	public static String toString(int[] sortedArr) {
 		StringBuilder sb = new StringBuilder("[");
--- SortAlgos/src/com/wilson/sorting/SortEntry.java
+++ SortAlgos/src/com/wilson/sorting/SortEntry.java
@@ -6,12 +6,21 @@ public class SortEntry {
 		
 		// BUBBLE SORT
 		int[] unsorted1 = new int[] {55, 2, 29, -1, 0, 11, 27, -13, 18};
-		int[] unsorted2 = new int[] {123, -22, -76, 49, 0, 33, 47, 1, 14, -3, 90, 18};
-		int[] sorted1 = BubbleSort.sort(unsorted1);
-		int[] sorted2 = BubbleSort.sort(unsorted2);
+		int[] unsorted2 = new int[] {123, -22, -76, 49, 0, 33, 47, 1, 14, -3, 90, 49, 18};
+		int[] unsorted3 = new int[] {33, -180, -3, 73, 11, 39, 0, -1, 85, 38};
+		int[] unsorted4 = new int[] {3, 23, 398, 88, -11, -92, 0, 33, 82, -3, 8, 83};
+		int[] sorted1 = BubbleSort.sort(unsorted1, "ascending");
+		int[] sorted2 = BubbleSort.sort(unsorted2, "ascending");
+		int[] sorted3 = BubbleSort.sort(unsorted3, "descending");
+		int[] sorted4 = BubbleSort.sort(unsorted4, "descending");
 		System.out.println("Sorted 1: " + BubbleSort.toString(sorted1));
 		System.out.println("Sorted 2: " + BubbleSort.toString(sorted2));
-		//
+		System.out.println("");
+		System.out.println("Sorted 3: " + BubbleSort.toString(sorted3));
+		System.out.println("Sorted 4: " + BubbleSort.toString(sorted4));
+		
+		// SELECTION SORT
+		
 	}
 
 }
